1d8ee308b7fc6903e748a18ec800dee11fb18a75
Leonor Oliveira <leonor.mm.oliveira@datapoint.pt>
2025-06-06 15:43:35 +0000

    feat: skypulce done 7

--- SkyPulse/vite.config.ts
+++ SkyPulse/vite.config.ts
@@ -3,5 +3,5 @@ import angular from '@analogjs/vite-plugin-angular';
 
 export default defineConfig({
   plugins: [angular()],
-  base: '/leonoroliveira-4546/flips-challenge.github.io', 
+  base: '/flips-challenge.github.io/', 
 });
